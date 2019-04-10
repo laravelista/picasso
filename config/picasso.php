2e083af209843245a28263523bdca517ff73233d
Mario Bašić <mario@laravelista.hr>
2019-04-10 12:18:21 +0200

    Implemented dot notation for dimensions. Fixes #1.

--- config/picasso.php
+++ config/picasso.php
@@ -6,17 +6,27 @@ return [
         // Replace this with your own dimension requirements
         'example_dimension_name' => [
             'width' => 1920,
-            'heigh' => 1080,
-            'watermark' => false
+            'heigh' => 1080
+        ],
+
+        // Dot notation example.
+        // Usage: frontend.categories.index
+        'frontend' => [
+            'categories' => [
+                'index' => [
+                    'width' => 1280,
+                    'heigh' => 720
+                ],
+                'show' => [
+                    'width' => 1920,
+                    'heigh' => 1080
+                ],
+            ]
         ]
 
         // ...
     ],
 
-    'watermark' => [
-        // location of the watermark on disk
-        'src' => '...'
-    ],
-
+    // Use this variable to set the default image quality.
     'quality' => 60
 ];
